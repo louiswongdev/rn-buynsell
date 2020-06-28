283609496db327b165b7c43192e995afaf96b40d
Louis <louis@Louiss-Mac-mini.local>
2020-06-28 16:33:13 -0400

    Added swipe gesture to individual messages and delete handler for deleting messages from UI

--- app/components/MessagesScreen.js
+++ app/components/MessagesScreen.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 
 import ListItem from './ListItem';
 import Screen from './Screen';
 import ListItemSeparator from './ListItemSeparator';
+import ListItemDeleteAction from './ListItemDeleteAction';
 
-const messages = [
+const initialMessages = [
   {
     id: 1,
     title: 'T1',
@@ -21,6 +22,13 @@ const messages = [
 ];
 
 function MessagesScreen() {
+  const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleDelete = message => {
+    setMessages(messages.filter(m => m.id !== message.id));
+    console.log('message deleted');
+  };
   return (
     <Screen>
       <FlatList
@@ -31,10 +39,23 @@ function MessagesScreen() {
             title={item.title}
             subTitle={item.description}
             image={item.image}
-            onPress={() => console.log('Message selected', item)}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
           />
         )}
         ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 2,
+              title: 'T2',
+              description: 'D2',
+              image: require('../assets/louis.png'),
+            },
+          ]);
+        }}
       />
     </Screen>
   );
