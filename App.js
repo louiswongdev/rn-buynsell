052633955fb5758953b3dc8340170e27317b9ec8
Louis <louis@Louiss-Mac-mini.local>
2020-06-28 13:52:58 -0400

    Added ListingDetailsScreen, AppText and Card components

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import AppButton from './app/components/AppButton';
+import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ListingDetailsScreen />;
 }
 
 const styles = StyleSheet.create({
