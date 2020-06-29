220565ed7c83f31b2fbf98c9ee41dc8300bdcc91
Louis <louis@Louiss-Mac-mini.local>
2020-06-29 15:14:42 -0400

    Added AppPicker component

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Switch,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+} from 'react-native';
 
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
@@ -13,11 +20,27 @@ import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
 import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
+
+const categories = [
+  { label: 'Furniture', value: 1 },
+  { label: 'Clothing', value: 2 },
+  { label: 'Cameras', value: 3 },
+];
 
 export default function App() {
+  const [category, setCategory] = useState(categories[0]);
+
   return (
     <Screen>
-      <AppTextInput icon="email" placeholder="Username" />
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={item => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
 }
