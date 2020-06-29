470a1c4c0609444f9325adf1dc7ee4b181d04560
Louis <louis@Louiss-Mac-mini.local>
2020-06-29 17:23:20 -0400

    Move Form components into forms folder. Re-export those files in /forms/index.js

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import { StyleSheet, Image } from 'react-native';
-import { Formik } from 'formik';
 import * as Yup from 'yup';
 
-import AppButton from '../components/AppButton';
-import AppFormField from '../components/AppFormField';
-import AppTextInput from '../components/AppTextInput';
-import ErrorMessage from '../components/ErrorMessage';
 import Screen from '../components/Screen';
-import SubmitButton from '../components/SubmitButton';
-import AppForm from '../components/AppForm';
+import { AppFormField, AppForm, SubmitButton } from '../components/forms';
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label('Email'),
