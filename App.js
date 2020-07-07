257069d8dc5d0d6888d052c81365df33258b24f1
Louis <louis@Louiss-Mac-mini.local>
2020-07-07 11:18:52 -0400

    Persist user auth state when reloading app with help of expo-secure-store package

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, Button } from 'react-native';
 import Screen from './app/components/Screen';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import jwtDecode from 'jwt-decode';
+import { AppLoading } from 'expo';
 
 import AuthNavigator from './app/navigation/AuthNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
 import OfflineNotice from './app/components/OfflineNotice';
 import AuthContext from './app/auth/context';
+import authStorage from './app/auth/storage';
 
 export default function App() {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
+
+  const restoreToken = async () => {
+    const token = await authStorage.getToken();
+    if (!token) return;
+
+    setUser(jwtDecode(token));
+  };
+
+  if (!isReady) {
+    return (
+      <AppLoading startAsync={restoreToken} onFinish={() => setIsReady(true)} />
+    );
+  }
+
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
